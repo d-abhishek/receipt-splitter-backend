95c14326a349d60d7bb268ff66d3981fd83e51e9
Abhishek Dhekane <adhekane@stud.hs-heilbronn.de>
2026-01-19 16:14:08 +0100

    Setup postgreSQL db in docker container

--- src/main/java/com/abhishek/receiptsplitterbackend/ReceiptSplitterBackendApplication.java
+++ src/main/java/com/abhishek/receiptsplitterbackend/ReceiptSplitterBackendApplication.java
@@ -2,9 +2,8 @@ package com.abhishek.receiptsplitterbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ReceiptSplitterBackendApplication {
 
     public static void main(String[] args) {
