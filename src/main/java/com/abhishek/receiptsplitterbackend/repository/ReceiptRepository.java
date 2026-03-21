60f79619374f28286f5f47781425fc64e2bbbc85
Abhishek Dhekane <adhekane@stud.hs-heilbronn.de>
2026-03-21 20:29:38 +0100

    Update repository interfaces to use correct entity types

--- src/main/java/com/abhishek/receiptsplitterbackend/repository/ReceiptRepository.java
+++ src/main/java/com/abhishek/receiptsplitterbackend/repository/ReceiptRepository.java
@@ -1,9 +1,9 @@
 package com.abhishek.receiptsplitterbackend.repository;
 
-import com.abhishek.receiptsplitterbackend.entity.ReceiptItemSplit;
+import com.abhishek.receiptsplitterbackend.entity.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ReceiptRepository extends JpaRepository<ReceiptItemSplit, UUID> {
+public interface ReceiptRepository extends JpaRepository<Receipt, UUID> {
 }
--- src/main/java/com/abhishek/receiptsplitterbackend/repository/ReceiptSplitRepository.java
+++ src/main/java/com/abhishek/receiptsplitterbackend/repository/ReceiptSplitRepository.java
@@ -1,9 +1,9 @@
 package com.abhishek.receiptsplitterbackend.repository;
 
-import com.abhishek.receiptsplitterbackend.entity.Payment;
+import com.abhishek.receiptsplitterbackend.entity.ReceiptSplit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ReceiptSplitRepository  extends JpaRepository<Payment, UUID> {
+public interface ReceiptSplitRepository  extends JpaRepository<ReceiptSplit, UUID> {
 }
