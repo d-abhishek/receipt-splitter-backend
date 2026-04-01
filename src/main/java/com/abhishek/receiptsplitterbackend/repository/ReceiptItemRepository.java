0b572d5b4deccc470c398c30b8294549484086b4
Abhishek Dhekane <adhekane@stud.hs-heilbronn.de>
2026-04-01 16:40:02 +0200

    Add repository methods for retrieving and deleting receipt and group item splits

--- src/main/java/com/abhishek/receiptsplitterbackend/repository/ReceiptItemRepository.java
+++ src/main/java/com/abhishek/receiptsplitterbackend/repository/ReceiptItemRepository.java
@@ -3,7 +3,10 @@ package com.abhishek.receiptsplitterbackend.repository;
 import com.abhishek.receiptsplitterbackend.entity.ReceiptItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReceiptItemRepository extends JpaRepository<ReceiptItem, UUID> {
+
+    List<ReceiptItem> findByReceiptId(UUID receipt_id);
 }
