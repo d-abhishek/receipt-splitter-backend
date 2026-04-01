295f41447944633c32ed604de69cfd0f12613c2a
Abhishek Dhekane <adhekane@stud.hs-heilbronn.de>
2026-04-01 16:40:41 +0200

    Move HealthController to controller directory to improve project structure

--- src/main/java/com/abhishek/receiptsplitterbackend/controller/HealthController.java
+++ src/main/java/com/abhishek/receiptsplitterbackend/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.abhishek.receiptsplitterbackend;
+package com.abhishek.receiptsplitterbackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
