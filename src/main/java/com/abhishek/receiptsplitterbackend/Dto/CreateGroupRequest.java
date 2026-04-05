58e438ef1f91238ee2ec5c113508864becf1a350
Abhishek Dhekane <adhekane@stud.hs-heilbronn.de>
2026-04-05 19:35:30 +0200

    Refactor CreateGroupRequest to use Lombok for boilerplate code reduction

--- src/main/java/com/abhishek/receiptsplitterbackend/Dto/CreateGroupRequest.java
+++ src/main/java/com/abhishek/receiptsplitterbackend/Dto/CreateGroupRequest.java
@@ -1,34 +1,13 @@
 package com.abhishek.receiptsplitterbackend.Dto;
 
+import lombok.Data;
+
 import java.util.List;
 import java.util.UUID;
 
+@Data
 public class CreateGroupRequest {
 
     private String groupName;
     private List<UUID> memberIds;
-
-
-    public CreateGroupRequest(String groupName, List<UUID> memberIds) {
-        this.groupName = groupName;
-        this.memberIds = memberIds;
-    }
-
-    // Getters and Setters
-
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
-
-    public List<UUID> getMemberIds() {
-        return memberIds;
-    }
-
-    public void setMemberIds(List<UUID> memberIds) {
-        this.memberIds = memberIds;
-    }
 }
