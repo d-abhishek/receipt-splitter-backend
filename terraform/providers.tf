400592d926e4c494a48f1288fae2720ccc5bfe82
Abhishek Dhekane <adhekane@stud.hs-heilbronn.de>
2026-04-26 21:16:14 +0200

    Add Terraform configuration for Azure resources including AKS, ACR, and PostgreSQL

---
+++ terraform/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+
+variable "resource_location" {
+  type        = string
+  default     = "polandcentral"
+  description = "Location of the resource group."
+}
